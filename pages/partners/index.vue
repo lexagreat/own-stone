853e8a2f917e4b7bd73a13d15acce6c9b16223fb
lexagreat <9608798010@mail.ru>
2024-11-25 19:22:42 +0300

    fix

---
+++ pages/partners/index.vue
@@ -0,0 +1,13 @@
+<template>
+   <main class="main">
+      <SectionsPartnersHero />
+      <SectionsPartnersFeatures />
+      <SectionsPartnersCollab />
+      <SectionsPartnersProcess />
+      <SectionsPartnersCases />
+      <SectionsPartnersCalculator />
+      <SectionsPartnersControl />
+
+   </main>
+</template>
+<script setup></script>
