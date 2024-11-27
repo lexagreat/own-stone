ed71bbf0f2746215c74ccbbb7ad3b89d8829479a
lexagreat <9608798010@mail.ru>
2024-11-27 12:48:41 +0300

    fix

--- components/sections/partners/hero.vue
+++ components/sections/partners/hero.vue
@@ -6,7 +6,7 @@
       <div class="container">
          <div class="sell-hero__wrapper">
             <SectionsBreadcrumbs :path="path" />
-            <h1 class="sell-hero__title h1 light-title">присоединяйтесь <span>к партнерской программе</span> </h1>
+            <h1 class="sell-hero__title h1 light-title">присоединяйтесь <br> <span>к партнерской программе</span> </h1>
             <div class="sell-hero__main">
                <p>И получайте вознаграждение в размере 30% от общей комиссии по факту реализованной сделки </p>
                <UiButton class="white">Стать партнером</UiButton>
