a8bfcb6ef3c02d498237d4ff49663d623d2cf005
lexagreat <9608798010@mail.ru>
2024-12-10 23:45:23 +0300

    fix

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,8 +1,8 @@
 import { gsap } from "gsap";
-
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-
 gsap.registerPlugin(ScrollTrigger);
+
+import { slideHide, slideShow } from "#build/imports";
 
 export const parallaxAnimation = () => {
    // console.log("parallax anim");
@@ -86,4 +86,129 @@ export const aboutValueAnim = () => {
          },
       });
    });
+};
+
+export const servicesAnimation = () => {
+   const bgs = document.querySelectorAll(".services-section__bg");
+   const section = document.querySelector(".services-section");
+   const spoilers = document.querySelectorAll(".services-section__content");
+   const headers = document.querySelectorAll(".services-section__header");
+   const sectionHeight = section.clientHeight;
+   gsap.to(section, {
+      scrollTrigger: {
+         trigger: section, // элемент, который должен запускать анимацию
+         start: "top top", // Точка начала анимации
+         end: `+=${sectionHeight}`, // Конец анимации через высоту секции
+         endTrigger: section, // указание родителя для окончания анимации
+         // markers: true, // включить маркеры для визуальной отладки
+         scrub: 1.5,
+      },
+      onRefresh: (self) => {
+         bgs.forEach((item) => {
+            item.classList.remove("hidden");
+         });
+         slideShow(spoilers[0], 600);
+         slideHide(spoilers[1], 600);
+         headers[0].classList.add("hidden");
+         headers[1].classList.remove("hidden");
+      },
+      // onUpdate: function () {
+      //    // Проверяем прогресс анимации
+      //    const progress = this.ratio;
+      //    if (progress >= 0 && progress < 0.25) {
+      //       bgs.forEach((item) => {
+      //          item.classList.remove("active");
+      //       });
+      //       bgs[0].classList.add("active");
+      //       bgs[0].style.transform = `translate(0, ${-100 * this.ratio}%)`;
+      //    }
+      //    if (progress >= 0.25 && progress < 0.45) {
+      //       bgs.forEach((item) => {
+      //          item.classList.remove("active");
+      //       });
+      //       bgs[1].classList.add("active");
+      //       bgs[1].style.transform = `translate(0, ${
+      //          -100 * (this.ratio - 0.25)
+      //       }%)`;
+      //    }
+      //    if (progress >= 0.45 && progress < 0.65) {
+      //       bgs.forEach((item) => {
+      //          item.classList.remove("active");
+      //       });
+      //       bgs[2].classList.add("active");
+      //       bgs[2].style.transform = `translate(0, ${
+      //          -100 * (this.ratio - 0.5)
+      //       }%)`;
+      //    }
+      //    if (progress >= 0.65) {
+      //       bgs.forEach((item) => {
+      //          item.classList.remove("active");
+      //       });
+      //       bgs[3].classList.add("active");
+      //       bgs[3].style.transform = `translate(0, ${
+      //          -100 * (this.ratio - 0.65)
+      //       }%)`;
+      //    }
+      onUpdate: function () {
+         // Проверяем прогресс анимации
+         const progress = this.ratio;
+         if (progress >= 0 && progress < 0.25) {
+            bgs.forEach((item) => {
+               item.classList.remove("hidden");
+            });
+            slideShow(spoilers[0], 600);
+            slideHide(spoilers[1], 600);
+            headers[0].classList.add("hidden");
+            headers[1].classList.remove("hidden");
+            bgs[0].style.transform = `translate(0, ${-100 * this.ratio}%)`;
+         }
+         if (progress >= 0.25 && progress < 0.5) {
+            slideHide(spoilers[0], 600);
+            slideHide(spoilers[2], 600);
+            slideShow(spoilers[1], 600);
+            headers[0].classList.remove("hidden");
+            headers[2].classList.remove("hidden");
+            headers[1].classList.add("hidden");
+            bgs[0].classList.add("hidden");
+            bgs[1].classList.remove("hidden");
+            bgs[1].style.transform = `translate(0, ${
+               -100 * (this.ratio - 0.25)
+            }%)`;
+         }
+         if (progress >= 0.5 && progress < 0.72) {
+            slideHide(spoilers[1], 600);
+            slideHide(spoilers[3], 600);
+            slideShow(spoilers[2], 600);
+            headers[1].classList.remove("hidden");
+            headers[3].classList.remove("hidden");
+            headers[2].classList.add("hidden");
+            bgs[1].classList.add("hidden");
+            bgs[2].classList.remove("hidden");
+            bgs[2].style.transform = `translate(0, ${
+               -100 * (this.ratio - 0.5)
+            }%)`;
+         }
+         if (progress >= 0.72) {
+            slideHide(spoilers[2], 600);
+            slideShow(spoilers[3], 600);
+            headers[2].classList.remove("hidden");
+            headers[3].classList.add("hidden");
+            bgs[2].classList.add("hidden");
+            bgs[3].style.transform = `translate(0, ${
+               -100 * (this.ratio - 0.72)
+            }%)`;
+         }
+      },
+   });
+   // gsap.to(bgs[0], {
+   //    scrollTrigger: {
+   //       trigger: section, // элемент, который должен запускать анимацию
+   //       start: "top top", // Точка начала анимации
+   //       end: `+=100vh`, // Конец анимации через высоту секции
+   //       endTrigger: section, // указание родителя для окончания анимации
+   //       // markers: true, // включить маркеры для визуальной отладки
+   //       scrub: 1.5,
+   //    },
+   //    y: "-10%",
+   // });
 };
--- pages/services/index.vue
+++ pages/services/index.vue
@@ -1,4 +1,110 @@
 <template>
-   <div></div>
+   <main class="main services-page">
+      <SectionsBreadcrumbs :path="path" />
+      <section class="services-section">
+         <div class="services-section__bgs">
+            <img class="services-section__bg" v-for="item in services" :key="item" :src="item.image" alt="">
+         </div>
+         <div class="services-section__card">
+            <ul class="services-section__list">
+               <li class="services-section__item" v-for="item in services" :key="item">
+                  <div class="services-section__header">
+                     <div class="circle">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                           <path fill-rule="evenodd" clip-rule="evenodd"
+                              d="M8.45179 3.50049V11.6498L11.7531 8.31669L12.2505 8.80929L8.10179 12.9978L3.95312 8.80929L4.45046 8.31669L7.75179 11.6498V3.50049H8.45179Z"
+                              fill="#181818" />
+                        </svg>
+                     </div>
+                     <h2 class="h3" v-html="item.title"></h2>
+                  </div>
+                  <div class="services-section__content collapse">
+                     <div>
+                        <h2 class="h1 dark-title" v-html="item.title"></h2>
+                        <div class="services-section__description">
+                           <div class="body-text" v-html="item.text"></div>
+                           <UiButton v-if="item?.isBtn" class="black">Оставить заявку</UiButton>
+                        </div>
+                     </div>
+                  </div>
+               </li>
+            </ul>
+         </div>
+      </section>
+   </main>
 </template>
-<script setup></script>
+<script setup>
+import { servicesAnimation } from '~/assets/js/animations';
+
+const path = ref([
+   {
+      name: "Главная",
+      to: "/"
+   },
+   {
+      name: "Услуги",
+      to: ""
+   },
+])
+
+const services = ref([
+   {
+      image: "/img/services/service0.jpeg",
+      title: "Услуги",
+      text: "Мы предлагаем полный комплекс профессиональных услуг в сфере недвижимости, как на первичном, так и на вторичном рынке, работая с объектами жилого и коммерческого назначения."
+   },
+   {
+      isBtn: true,
+      image: "/img/services/service1.jpeg",
+      title: "Покупка <span> недвижимости</span>",
+      text: `<ul>
+   <li>Выявление точного запроса </li>
+   <li>Персонализированная подборка лотов </li>
+   <li>Организация и проведение встреч на объекте </li>
+   <li>Полное сопровождение сделки: подготовка документов, юридическая проверка, контроль на каждом этапе </li>
+   <li>Одобрение ипотеки, подбор выгодной банковской программы </li>
+   <li>Регистрация в Росреестре </li>
+</ul>`
+   },
+   {
+      isBtn: true,
+      image: "/img/services/service2.jpeg",
+      title: "Продажа <span> недвижимости</span>",
+      text: `
+<ul>
+   <li>Аналитика по рынку: конкурентный анализ, локационный спрос, ценообразование </li>
+   <li>Профессиональная оценка вашей недвижимости</li>
+   <li>Организация фото и видео-съемки объекта</li>
+   <li>Упаковка и размещение объявления </li>
+   <li>Маркетинговая поддержка, продвижение </li>
+   <li>Организация просмотров объекта</li>
+   <li>Юридическое сопровождение сделки</li>
+</ul>
+      `
+   },
+   {
+      isBtn: true,
+      image: "/img/services/service3.jpeg",
+      title: "Инвестиции <span> в недвижимость</span>",
+      text: `
+<ul>
+   <li>Аналитика по рынку: конкурентный анализ, локационный спрос, ценообразование </li>
+   <li>Профессиональная оценка вашей недвижимости</li>
+   <li>Организация фото и видео-съемки объекта</li>
+   <li>Упаковка и размещение объявления </li>
+   <li>Маркетинговая поддержка, продвижение </li>
+   <li>Организация просмотров объекта</li>
+   <li>Юридическое сопровождение сделки</li>
+</ul>
+      `
+   },
+])
+
+
+onMounted(() => {
+   setTimeout(() => {
+      servicesAnimation()
+
+   }, 300)
+})
+</script>
