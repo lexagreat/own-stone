f58980fcec7e04176b34700de9aa3b42ef027bd3
lexagreat <9608798010@mail.ru>
2025-01-29 14:54:19 +0300

    fix

--- components/maps/contacts.vue
+++ components/maps/contacts.vue
@@ -1,12 +1,24 @@
 <template>
-   <yandex-map real-settings-location :settings="{
+   <yandex-map :settings="{
       location: {
-         center: [37.617644, 55.755819],
+         center: [55.745838, 37.598672],
          zoom: 9,
-         duration: 2500,
-      },
+         duration: 2500
+      }
    }">
       <yandex-map-default-scheme-layer :settings="{ theme: 'dark' }" />
+
+      <!-- Маркер -->
+      <yandex-map-marker :settings="{
+         coordinates: [55.745838, 37.598672],
+         icon: {
+            layout: 'default#image',
+            imageHref: '/fav.svg', // URL иконки
+            imageSize: [40, 40], // Размер иконки
+            imageOffset: [-20, -40] // Смещение, чтобы центрировать
+         }
+      }" />
+
       <yandex-map-controls :settings="{ position: 'right' }">
          <yandex-map-zoom-control />
       </yandex-map-controls>
@@ -20,5 +32,6 @@ import {
    YandexMapControls,
    YandexMapDefaultSchemeLayer,
    YandexMapZoomControl,
+   YandexMapMarker
 } from "vue-yandex-maps";
 </script>
