51ca4931c0db14eec3e2b69cacc4331ef2c59542
lexagreat <9608798010@mail.ru>
2025-01-30 14:49:57 +0300

    fxi

--- components/maps/project.vue
+++ components/maps/project.vue
@@ -2,20 +2,20 @@
    <div id="map" style="width:100%; height:100%"></div>
 </template>
 <script setup>
-onMounted(() => {
-   var map;
+// onMounted(() => {
+//    var map;
 
-   DG.then(function () {
-      map = DG.map('map', {
-         center: [54.98, 82.89],
-         zoom: 13
-      });
+//    DG.then(function () {
+//       map = DG.map('map', {
+//          center: [54.98, 82.89],
+//          zoom: 13
+//       });
 
-      DG.marker([54.98, 82.91]).addTo(map);
-      DG.marker([54.98, 82.92]).addTo(map);
-      DG.marker([54.98, 82.89]).addTo(map).bindPopup('Вы кликнули по мне!');
-   });
+//       DG.marker([54.98, 82.91]).addTo(map);
+//       DG.marker([54.98, 82.92]).addTo(map);
+//       DG.marker([54.98, 82.89]).addTo(map).bindPopup('Вы кликнули по мне!');
+//    });
 
-})
+// })
 
 </script>
