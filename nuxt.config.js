8e4d35bd6328d22e9efd53b395bb3aa683e516b9
lexagreat <9608798010@mail.ru>
2025-06-02 15:16:05 +0300

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,9 +25,35 @@ export default defineNuxtConfig({
                content: "telephone=no",
             },
          ],
-         script: [],
+         script: [
+            {
+               hid: "yandex-metrika",
+               innerHTML: `
+          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+          ym(97044815, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true
+          });
+        `,
+               type: "text/javascript",
+               charset: "utf-8",
+            },
+         ],
+         noscript: [
+            {
+               hid: "yandex-metrika-noscript",
+               innerHTML: `<div><img src="https://mc.yandex.ru/watch/97044815" style="position:absolute; left:-9999px;" alt="" /></div>`,
+            },
+         ],
       },
    },
+
    modules: ["nuxt-swiper", "@pinia/nuxt", "vue-yandex-maps/nuxt"],
    yandexMaps: {
       apikey: "bd41a6a2-c519-457e-9050-57a1ca739237",
