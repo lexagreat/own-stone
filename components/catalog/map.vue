3ce51ad4e67a59a515dd4a36f1acdf17d5c59991
alekseyosipov <9608798010@mail.ru>
2025-09-03 13:46:35 +0300

    fix

--- components/catalog/map.vue
+++ components/catalog/map.vue
@@ -1,183 +1,191 @@
 <template>
-   <div class="catalog-card loading" v-if="!products?.length" style="height: 640px;max-width: 100%;"></div>
-   <div class="catalog-map__wrapper">
-      <yandex-map ref="map" class="catalog-map" v-if="products?.length" :settings="{
-         location: {
-            center: parseCoords(products[0]) || [37.6173, 55.7558], // Москва по умолчанию
-            zoom: 9,
-            duration: 2500
-         },
-         mapsRenderWaitDuration: 5000
-      }">
-         <yandex-map-default-scheme-layer :settings="{ theme: 'light' }" />
-         <yandex-map-default-features-layer />
+  <div class="catalog-card loading" v-if="!products?.length" style="height: 640px; max-width: 100%"></div>
+  <div class="catalog-map__wrapper">
+    <yandex-map
+      ref="map"
+      class="catalog-map"
+      v-if="products?.length"
+      :settings="{
+        location: {
+          center: parseCoords(products[0]) || [37.6173, 55.7558], // Москва по умолчанию
+          zoom: 9,
+          duration: 2500,
+        },
+        mapsRenderWaitDuration: 5000,
+      }"
+    >
+      <yandex-map-default-scheme-layer :settings="{ theme: 'light' }" />
+      <yandex-map-default-features-layer />
 
-         <!-- Кластеризатор с дополнительными настройками -->
-         <yandex-map-clusterer :settings="{
+      <!-- Кластеризатор с дополнительными настройками -->
+      <yandex-map-clusterer :settings="{}">
+        <template v-for="(item, index) in products" :key="index">
+          <yandex-map-marker v-if="parseCoords(item)" :settings="{ coordinates: parseCoords(item) }">
+            <div class="marker circle" @click="onMarkerClick(index)">
+              {{ item.count || 1 }}
+            </div>
+          </yandex-map-marker>
+        </template>
+        <template #cluster="{ length, clusterer, coordinates }">
+          <div class="cluster">
+            <div class="marker circle" @click="onClusterClick({ length, clusterer, coordinates })">
+              {{ length }}
+            </div>
+          </div>
+        </template>
+      </yandex-map-clusterer>
 
-         }">
-            <template v-for="(item, index) in products" :key="index">
-               <yandex-map-marker v-if="parseCoords(item)" :settings="{ coordinates: parseCoords(item) }">
-                  <div class="marker circle" @click="onMarkerClick(index)">
-                     {{ item.count || 1 }}
-                  </div>
-               </yandex-map-marker>
-            </template>
-            <template #cluster="{ length, clusterer, coordinates }">
-               <div class="cluster">
-                  <div class="marker circle" @click="onClusterClick({ length, clusterer, coordinates })">
-                     {{ length }}
-                  </div>
-               </div>
-            </template>
-         </yandex-map-clusterer>
-
-         <yandex-map-controls :settings="{ position: 'right' }">
-            <yandex-map-zoom-control />
-         </yandex-map-controls>
-      </yandex-map>
-      <CatalogListsMap :products="currentProducts" :category="category" @openForm="onForm"
-         @close="currentProducts = []" />
-   </div>
+      <yandex-map-controls :settings="{ position: 'right' }">
+        <yandex-map-zoom-control />
+      </yandex-map-controls>
+    </yandex-map>
+    <CatalogListsMap
+      :products="currentProducts"
+      :category="category"
+      @openForm="onForm"
+      @close="currentProducts = []"
+    />
+  </div>
 </template>
 
 <script setup>
 import {
-   YandexMap,
-   YandexMapControls,
-   YandexMapDefaultSchemeLayer,
-   YandexMapDefaultFeaturesLayer,
-   YandexMapZoomControl,
-   YandexMapMarker,
-   YandexMapClusterer
-} from "vue-yandex-maps";
-import { useCatalog } from "~/store/catalog";
-const catalog = useCatalog()
+  YandexMap,
+  YandexMapControls,
+  YandexMapDefaultSchemeLayer,
+  YandexMapDefaultFeaturesLayer,
+  YandexMapZoomControl,
+  YandexMapMarker,
+  YandexMapClusterer,
+} from 'vue-yandex-maps';
+import { useCatalog } from '~/store/catalog';
+const catalog = useCatalog();
 const props = defineProps({
-   searchUrl: String,
-   category: Number
+  searchUrl: String,
+  category: Number,
 });
-const products = ref([])
+const products = ref([]);
 const getProducts = async () => {
-   let base = "catalog";
-   const { data } = await useBaseFetch(base + props.searchUrl + `&pagination[pageSize]=10000`);
-   // console.log('SEARCH CATALOG MAP', data);
-   products.value = data;
-   // if (!data?.length) {
-   //    products.value = [];
-   //    return;
-   // }
-   // if (data[0]?.coords) {
-   //    products.value = data;
-   //    return
-   // }
-   // products.value = [...new Set(data.map(item => item.proekty))];
-}
-watch(() => props.searchUrl, async () => {
-   await getProducts()
-}, {
-   immediate: true
-})
-const currentProducts = ref([])
+  let base = 'catalog';
+  const { data } = await useBaseFetch(base + props.searchUrl + `&pagination[pageSize]=10000`);
+  // console.log('SEARCH CATALOG MAP', data);
+  products.value = data;
+  // if (!data?.length) {
+  //    products.value = [];
+  //    return;
+  // }
+  // if (data[0]?.coords) {
+  //    products.value = data;
+  //    return
+  // }
+  // products.value = [...new Set(data.map(item => item.proekty))];
+};
+watch(
+  () => props.searchUrl,
+  async () => {
+    await getProducts();
+  },
+  {
+    immediate: true,
+  },
+);
+const currentProducts = ref([]);
 
 // Функция парсинга координат
 const parseCoords = (item) => {
-   let coords;
-   if (!item?.coords && !item?.proekty) return null;
+  let coords;
+  if (!item?.coords && !item?.proekty) return null;
 
-   if (item?.proekty) {
-      coords = item?.proekty.coords;
-   } else {
-      coords = item?.coords;
-   }
-   try {
-      return typeof coords === "string" ? JSON.parse(coords) : coords;
-   } catch (error) {
-      console.error("Ошибка парсинга координат:", coords);
-      return null;
-   }
+  if (item?.proekty) {
+    coords = item?.proekty.coords;
+  } else {
+    coords = item?.coords;
+  }
+  try {
+    return typeof coords === 'string' ? JSON.parse(coords) : coords;
+  } catch (error) {
+    console.error('Ошибка парсинга координат:', coords);
+    return null;
+  }
 };
 
-
 const onMarkerClick = (index) => {
-   currentProducts.value = [products.value[index]]
-   // console.log('Элементы в маркере:', currentProducts.value);
-
-}
+  currentProducts.value = [products.value[index]];
+  // console.log('Элементы в маркере:', currentProducts.value);
+};
 const onClusterClick = ({ length, clusterer, coordinates }) => {
-   // Получаем все маркеры внутри кластера через `clusterer.getChildren()`
-   const clusterMarkers = clusterer ? clusterer.features : [];
+  // Получаем все маркеры внутри кластера через `clusterer.getChildren()`
+  const clusterMarkers = clusterer ? clusterer.features : [];
 
-   // Извлекаем координаты всех маркеров в кластере
-   const clusterCoords = clusterMarkers.map(marker => marker.geometry.coordinates);
+  // Извлекаем координаты всех маркеров в кластере
+  const clusterCoords = clusterMarkers.map((marker) => marker.geometry.coordinates);
 
-   // Фильтруем продукты, координаты которых совпадают с координатами маркеров в кластере
-   const itemsInCluster = products.value.filter(product => {
-      const productCoords = parseCoords(product);
-      return productCoords && clusterCoords.some(coords =>
-         coords[0] === productCoords[0] && coords[1] === productCoords[1]
-      );
-   });
+  // Фильтруем продукты, координаты которых совпадают с координатами маркеров в кластере
+  const itemsInCluster = products.value.filter((product) => {
+    const productCoords = parseCoords(product);
+    return (
+      productCoords && clusterCoords.some((coords) => coords[0] === productCoords[0] && coords[1] === productCoords[1])
+    );
+  });
 
-   currentProducts.value = itemsInCluster
-   // console.log('Элементы в кластере:', currentProducts.value);
-}
+  currentProducts.value = itemsInCluster;
+  // console.log('Элементы в кластере:', currentProducts.value);
+};
 
-const emit = defineEmits(['openForm'])
+const emit = defineEmits(['openForm']);
 
 const onForm = (id) => {
-   emit('openForm', id)
-}
+  emit('openForm', id);
+};
 </script>
 
 <style scoped>
 .pin {
-   cursor: pointer;
+  cursor: pointer;
 }
 
 .catalog-map__wrapper {
-   position: relative;
-   overflow: hidden;
-   margin-bottom: 160px;
-   height: fit-content;
+  position: relative;
+  overflow: hidden;
+  margin-bottom: 160px;
+  height: fit-content;
 
-   @media(max-width: 1024px) {
-      margin-bottom: 80px;
-      overflow: unset;
-   }
+  @media (max-width: 1024px) {
+    margin-bottom: 80px;
+    overflow: unset;
+  }
 }
 
 .catalog-map {
-   border-radius: 8px;
-   overflow: hidden;
-   height: 640px !important;
-   width: 100% !important;
+  border-radius: 8px;
+  overflow: hidden;
+  height: 640px !important;
+  width: 100% !important;
 
-   @media(max-width: 1024px) {
+  @media (max-width: 1024px) {
+    margin-left: -20px;
+    margin-right: -20px;
+    width: 100vw !important;
+    border-radius: 0 !important;
+  }
 
-      margin-left: -20px;
-      margin-right: -20px;
-      width: 100vw !important;
-      border-radius: 0 !important;
-   }
-
-   @media(max-width: 568px) {
-      margin-left: -12px;
-      margin-right: -12px;
-   }
+  @media (max-width: 568px) {
+    margin-left: -12px;
+    margin-right: -12px;
+  }
 }
 
 .marker {
-   cursor: pointer;
-   width: 40px;
-   height: 40px;
-   background-color: #4F0014;
-   color: white;
-   display: flex;
-   justify-content: center;
-   align-items: center;
-   border-radius: 50%;
-   top: 100px;
-   translate: -50% -50%;
+  cursor: pointer;
+  width: 32px;
+  height: 32px;
+  background-color: rgba(79, 0, 20, 0.8);
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  top: 100px;
+  translate: -50% -50%;
 }
 </style>
--- components/maps/project.vue
+++ components/maps/project.vue
@@ -1,167 +1,162 @@
 <template>
-
-   <yandex-map :settings="{
+  <yandex-map
+    :settings="{
       location: {
-         center: center,
-         zoom: 14,
-         duration: 2500,
+        center: center,
+        zoom: 14,
+        duration: 2500,
       },
       behaviors: enabledBehaviors,
       mapsRenderWaitDuration: 5000, // Increase timeout duration (default is 2000ms)
+    }"
+  >
+    <yandex-map-listener :settings="{ onWheel: onUpdate }" />
+    <yandex-map-default-scheme-layer :settings="{ theme: 'light' }" />
 
-   }">
-      <yandex-map-listener :settings="{ onWheel: onUpdate }" />
-      <yandex-map-default-scheme-layer :settings="{ theme: 'light' }" />
+    <yandex-map-default-features-layer />
 
-      <yandex-map-default-features-layer />
+    <yandex-map-marker
+      position="top-center left-center"
+      v-if="JSON.parse(projectMarker?.coords)"
+      :settings="{ coordinates: JSON.parse(projectMarker?.coords) }"
+      @click="toggleBalloon(index)"
+    >
+      <div v-if="activeMarker === index && projectMarker.title?.length" class="balloon project">
+        {{ projectMarker.title }}
+      </div>
+      <div class="circle pin project" v-else>
+        <component :is="projectMarker.icon" />
+      </div>
+    </yandex-map-marker>
 
+    <!-- Кластеризатор с дополнительными настройками -->
+    <yandex-map-clusterer :settings="{}">
+      <template v-for="(item, index) in info" :key="index">
+        <yandex-map-marker
+          position="top-center left-center"
+          v-if="JSON.parse(item?.coords) && item?.id"
+          :settings="{ coordinates: JSON.parse(item?.coords) }"
+          @click="toggleBalloon(index)"
+        >
+          <div v-if="activeMarker === index && item.title?.length" class="balloon" :class="{ project: !item?.id }">
+            {{ item.title }}
+          </div>
+          <div class="circle pin" :class="{ project: !item?.id }" v-else>
+            <component :is="item.icon" />
+          </div>
+        </yandex-map-marker>
+      </template>
 
-      <yandex-map-marker position="top-center left-center" v-if="JSON.parse(projectMarker?.coords)"
-         :settings="{ coordinates: JSON.parse(projectMarker?.coords) }" @click="toggleBalloon(index)">
+      <template #cluster="{ length, clusterer, coordinates }">
+        <div class="cluster">
+          <div class="marker circle">
+            {{ length }}
+          </div>
+        </div>
+      </template>
+    </yandex-map-clusterer>
 
-         <div v-if="activeMarker === index && projectMarker.title?.length" class="balloon project">
-            {{ projectMarker.title }}
-         </div>
-         <div class="circle pin project" v-else>
-            <component :is="projectMarker.icon" />
-         </div>
-
-      </yandex-map-marker>
-
-
-      <!-- Кластеризатор с дополнительными настройками -->
-      <yandex-map-clusterer :settings="{
-
-      }">
-         <template v-for="(item, index) in info" :key="index">
-            <yandex-map-marker position="top-center left-center" v-if="JSON.parse(item?.coords) && item?.id"
-               :settings="{ coordinates: JSON.parse(item?.coords) }" @click="toggleBalloon(index)">
-
-               <div v-if="activeMarker === index && item.title?.length" class="balloon" :class="{ project: !item?.id }">
-                  {{ item.title }}
-               </div>
-               <div class="circle pin" :class="{ project: !item?.id }" v-else>
-                  <component :is="item.icon" />
-               </div>
-
-            </yandex-map-marker>
-         </template>
-
-         <template #cluster="{ length, clusterer, coordinates }">
-            <div class="cluster">
-               <div class="marker circle">
-                  {{ length }}
-               </div>
-            </div>
-         </template>
-      </yandex-map-clusterer>
-
-
-
-
-      <yandex-map-controls :settings="{ position: 'right' }">
-         <yandex-map-zoom-control />
-      </yandex-map-controls>
-   </yandex-map>
+    <yandex-map-controls :settings="{ position: 'right' }">
+      <yandex-map-zoom-control />
+    </yandex-map-controls>
+  </yandex-map>
 </template>
 
 <script setup>
 import {
-   VueYandexMaps,
-   YandexMap,
-   YandexMapControls,
-   YandexMapDefaultSchemeLayer,
-   YandexMapDefaultFeaturesLayer, // 🔹 Импортируем слой объектов
-   YandexMapZoomControl,
-   YandexMapDefaultMarker,
-   YandexMapMarker,
-   YandexMapClusterer,
-   YandexMapListener
-} from "vue-yandex-maps";
+  VueYandexMaps,
+  YandexMap,
+  YandexMapControls,
+  YandexMapDefaultSchemeLayer,
+  YandexMapDefaultFeaturesLayer, // 🔹 Импортируем слой объектов
+  YandexMapZoomControl,
+  YandexMapDefaultMarker,
+  YandexMapMarker,
+  YandexMapClusterer,
+  YandexMapListener,
+} from 'vue-yandex-maps';
 const props = defineProps({
-   info: Array,
-   center: Array
-})
+  info: Array,
+  center: Array,
+});
 
 // Отслеживание активного маркера
 const activeMarker = ref(null);
 
 // Открытие/закрытие балуна
 const toggleBalloon = (index) => {
-   activeMarker.value = activeMarker.value === index ? null : index;
+  activeMarker.value = activeMarker.value === index ? null : index;
 };
 const projectMarker = computed(() => {
-   return props.info.find(item => !item.id)
-})
+  return props.info.find((item) => !item.id);
+});
 const enabledBehaviors = ref(['drag', 'pinchZoom', 'dblClick']);
 </script>
 
-
 <style lang="scss">
 .pin {
-   cursor: pointer;
-   --size: 48px;
-   background-color: #181818;
+  cursor: pointer;
+  --size: 32px;
+  background-color: #181818cc;
 
-   &.project {
-      background-color: unset;
-
-
-   }
+  &.project {
+    background-color: unset;
+    --size: 48px;
+  }
 }
 
 .ymaps3x0--marker {
-   &:has(.balloon) {
-      z-index: 23 !important;
-   }
+  &:has(.balloon) {
+    z-index: 23 !important;
+  }
 }
 
 .balloon {
-   position: absolute;
-   background: #181818;
-   color: white;
-   padding: 10px;
-   border-radius: 4px;
-   cursor: pointer;
-   width: fit-content;
-   top: 0;
-   left: 0;
-   translate: -50% -50%;
-   line-height: 1.2;
+  position: absolute;
+  background: #181818;
+  color: white;
+  padding: 10px;
+  border-radius: 4px;
+  cursor: pointer;
+  width: fit-content;
+  top: 0;
+  left: 0;
+  translate: -50% -50%;
+  line-height: 1.2;
 
-   &.project {
-      background: #4F0014;
+  &.project {
+    background: #4f0014;
 
-      &::after {
-         background-color: #4F0014;
+    &::after {
+      background-color: #4f0014;
+    }
+  }
 
-      }
-   }
-
-   &::after {
-      content: "";
-      display: block;
-      clip-path: polygon(50% 100%, 0 0, 100% 0);
-      width: 20px;
-      aspect-ratio: 2;
-      background-color: #181818;
-      position: absolute;
-      top: 98%;
-      left: 50%;
-      translate: -50% 0;
-   }
+  &::after {
+    content: '';
+    display: block;
+    clip-path: polygon(50% 100%, 0 0, 100% 0);
+    width: 20px;
+    aspect-ratio: 2;
+    background-color: #181818;
+    position: absolute;
+    top: 98%;
+    left: 50%;
+    translate: -50% 0;
+  }
 }
 
 .marker {
-   cursor: pointer;
-   width: 40px;
-   height: 40px;
-   background-color: #181818;
-   color: white;
-   display: flex;
-   justify-content: center;
-   align-items: center;
-   border-radius: 50%;
-   translate: -50% -50%;
+  cursor: pointer;
+  width: 32px;
+  height: 32px;
+  background-color: #181818cc;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  translate: -50% -50%;
+  font-size: 16px;
 }
 </style>
